b7f3fe184761d9ffebe0f471d50d4bff761aa6cd
Wonjun Ko <wjko@korea.ac.kr>
2022-02-10 15:51:00 +0900

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,23 @@
+import os
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+# Pathes
+# path = "/home/wonjun/Desktop/pycharm-2017.2.3/PycharmProjects"
+data_path = "."
+# baseline_path = create_dir(path + "/Adversarial_Training/models/baselines")
+# adversarial_path = create_dir(path + "/Adversarial_Training/models/adversarial")
+# generation_path = create_dir(path + "/Adversarial_Training/figures/generation")
+# activation_path = create_dir(path + "/Adversarial_Training/figures/activation")
+# tsne_path = create_dir(path + "/Adversarial_Training/features")
+
+# Experiment conditions
+subject = 1
+batch_size = 64
+total_epoch = 5
+n_noise = 400
+learning_rate = 1e-5
+window_size = 512
